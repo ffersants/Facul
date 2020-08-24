480a045addfab5f46e45418fcd1cffa1d4728a72
Fernando Ferreira <fersants.nando.ferreira@gmail.com>
2020-08-23 23:29:41 -0300

    Alteração que foi para o fórum.

--- POB/Unidade I/Aula 2/Exercicio 2/BillsAndInterest.java
+++ POB/Unidade I/Aula 2/Exercicio 2/BillsAndInterest.java
@@ -1,18 +1,19 @@
-/*
-Implemente um programa em Java que calcule o juro de uma dívida que você contraiu no mês passado 
-no crediário de uma loja. A taxa de juros mensal e o valor da dívida serão fornecidos pelo usuário.
-*/
-import java.util.Scanner;
+// /*
+// Implemente um programa em Java que calcule o juro de uma dívida que você contraiu no mês passado 
+// no crediário de uma loja. A taxa de juros mensal e o valor da dívida serão fornecidos pelo usuário.
+// */
+    import java.util.Scanner;
 
-public class BillsAndInterest{
-    public static void main(String[] args){
-        double bill;
-        System.out.println("How mutch is the bill payment? ");
-        bill = new Scanner(System.in).nextDouble();
-        System.out.println("What's the interest in this bill in per cent?");
-        double interest = new Scanner(System.in).nextDouble();
-        
-        double interestCalculated = interest / 100 * bill;
-        System.out.println("Your interest is " + interestCalculated + " dinheiros! \n So the amout is " + bill + interestCalculated + " dinheiros!");
+    public class BillsAndInterest{
+        public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+            double bill;
+            System.out.println("Quando eh a divida? ");
+            bill = scanner.nextDouble();
+            System.out.println("Qual a porcentagem de juros? ");
+            double interest = scanner.nextDouble();
+            
+            double interestCalculated = interest / 100 * bill;
+            System.out.println("O juros calculado eh " + interestCalculated + " reais ao mes! \n A divida mais os juros fica no total de " + (bill + interestCalculated) + " reais!");
+        }
     }
-}
